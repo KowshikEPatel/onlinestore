17f4747cc35e03cd9addfa628247da2703790f44
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-09-10 20:24:29 +0530

    billing displayed in user menu

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useContext,useState} from 'react';
 import Img from './img1.jpg';
-import {Card, Container,Row,Col,Button} from 'react-bootstrap';
+import {Card, Container,Row,Col } from 'react-bootstrap';
 import { UserContext } from './Context';
 import axios from 'axios';
 
@@ -8,15 +8,15 @@ export default function User() {
     let contextdata = useContext(UserContext);
 
     const [billarray, setBillarray] = useState([]);
-    const [show, setShow] = useState({showvar:"none",shownum:-1});
-
-    const handleClose = () => setShow({showvar:"none",shownum:-1});
-    const handleShow = (number) => setShow({showvar:"block",shownum:number});
-
+//style={{display:show.showvar}}onClick={()=>{handleShow(index)}} 
+/*
+               
+               */
     
     useEffect(()=>{
         async function fetchData(){
             let res = await axios.get(`https://kp-onlinestore.herokuapp.com/bills/user/${contextdata.userData._id}`);
+            console.log(res.data);
             setBillarray(res.data);
         }
         fetchData()
@@ -26,12 +26,15 @@ export default function User() {
         <>
             <div style={{backgroundColor:'#006039',height:"30vh"}}>
                 <div className="jumbotron jumbotron-fluid" style={{padding:"100px 50px 0 0"}}>
+                    <div className="container">
+                        <h1 className="display-4 text-light">Hi {contextdata.userData.username}</h1>
+                    </div>
                     <img src={Img} alt="user face" width="175px" style={{float:"right"}}/>
                 </div>
             </div>
             {contextdata.userData.role==="Seller"?<Container style={{padding:"100px"}}>
                {billarray.map((e,index)=>{
-                   return   <Row key={index} onClick={()=>{handleShow(index)}} >
+                   return   <Row key={index}>
                                 <Col xs={5}>
                                         <Card>
                                             <Card.Title>
@@ -40,19 +43,19 @@ export default function User() {
                                             <Card.Text>
                                             Rs. {e.totalValue}
                                             </Card.Text>
+                                            <Card.Text>
+                                             {e.billRecipient}
+                                            </Card.Text>
+                                            <Card.Text>
+                                             {e.products.map((e,index)=>{
+                                                 return <Card.Text key={index}>{e.name}</Card.Text>
+                                             })}
+                                            </Card.Text>
+                                            
                                         </Card>
                                 </Col>
                             </Row>
                })}
-               <Row style={{display:show.showvar}}>
-                    <Col>
-                        <Card>
-                            <Card.Title>Bill {show.shownum+1}</Card.Title>
-                            <Card.Text>{JSON.parse(billarray)}</Card.Text>
-                            <Button onClick={handleClose}>click</Button>
-                        </Card>
-                    </Col>
-               </Row>
                </Container>:null}
             
         </>
