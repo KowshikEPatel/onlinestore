ad72962340db6a285e7bbd6410b22bbb29d6c7c1
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-09-10 18:08:20 +0530

    bill array

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,22 +1,27 @@
-import React,{useEffect,useContext} from 'react';
+import React,{useEffect,useContext,useState} from 'react';
 import Img from './img1.jpg';
-import {Container} from 'react-bootstrap';
+import {Card, Container,Row,Col} from 'react-bootstrap';
 import { UserContext } from './Context';
 import axios from 'axios';
 
 export default function User() {
     let contextdata = useContext(UserContext);
 
-    console.log(contextdata.userData.role)
+    const [billarray, setBillarray] = useState([]);
+    const [show, setShow] = useState({showvar:"none",shownum:-1});
+
+    const handleClose = () => setShow({showvar:"none",shownum:-1});
+    const handleShow = (number) => setShow({showvar:"block",shownum:number});
+
+    
     useEffect(()=>{
         async function fetchData(){
-            let res = await axios.get(`https://kp-onlinestore.herokuapp.com/bills/user/${contextdata.userData._id}`)
-            console.log(res);
-            console.log(res.data);
+            let res = await axios.get(`https://kp-onlinestore.herokuapp.com/bills/user/${contextdata.userData._id}`);
+            setBillarray(res.data);
         }
         fetchData()
         
-    },[contextdata.userData])
+    },[contextdata.userData]);
     return (
         <>
             <div style={{backgroundColor:'#006039',height:"30vh"}}>
@@ -24,9 +29,31 @@ export default function User() {
                     <img src={Img} alt="user face" width="175px" style={{float:"right"}}/>
                 </div>
             </div>
-            <Container style={{padding:"100px"}}>
-               
-            </Container>
+            {contextdata.userData.role==="Seller"?<Container style={{padding:"100px"}}>
+               {billarray.map((e,index)=>{
+                   return   <Row key={index} onClick={()=>{handleShow(index)}} >
+                                <Col xs={5}>
+                                        <Card>
+                                            <Card.Title>
+                                                Bill {index+1}
+                                            </Card.Title>
+                                            <Card.Text>
+                                            Rs. {e.totalValue}
+                                            </Card.Text>
+                                        </Card>
+                                </Col>
+                            </Row>
+               })}
+               <Row style={{display:show.showvar}}>
+                    <Col>
+                        <Card>
+                            <Card.Title>Bill {show.shownum+1}</Card.Title>
+                            <Card.Text>{JSON.parse(billarray)}</Card.Text>
+                        </Card>
+                    </Col>
+               </Row>
+               </Container>:null}
+            
         </>
     )
 }
